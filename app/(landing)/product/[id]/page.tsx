42abcfd78905e70819db836d049eb5c89c08ce03
frsyi <fahmarosyi@gmail.com>
2026-01-15 16:20:59 +0700

    feat: add price formatter utility for IDR currency

--- app/(landing)/product/[id]/page.tsx
+++ app/(landing)/product/[id]/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import ProductActions from "../../components/product-detail/product-actions";
+import priceFormatter from "@/app/utils/price-formatter";
 
 const ProductDetail = () => {
   return (
@@ -27,10 +28,7 @@ const ProductDetail = () => {
           traction.
         </p>
         <div className="text-primary text-[32px] font-semibold mb-12">
-          {Intl.NumberFormat("id-ID", {
-            style: "currency",
-            currency: "IDR",
-          }).format(458000)}
+          {priceFormatter(458000)}
         </div>
         <ProductActions />
       </div>

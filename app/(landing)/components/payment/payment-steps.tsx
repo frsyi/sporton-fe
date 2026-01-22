062f9e5ed8b8e73ce3bd41f6e206af9219c15081
frsyi <fahmarosyi@gmail.com>
2026-01-22 10:26:00 +0700

    feat: integrate banks fetch API

--- app/(landing)/components/payment/payment-steps.tsx
+++ app/(landing)/components/payment/payment-steps.tsx
@@ -6,12 +6,55 @@ import Button from "../ui/button";
 import { FiCheckCircle } from "react-icons/fi";
 import priceFormatter from "@/app/utils/price-formatter";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useCartStore } from "@/app/hooks/use-cart-store";
 
 const PaymentSteps = () => {
   const { push } = useRouter();
+  const { items, customerInfo } = useCartStore();
+  const [file, setFile] = useState<File | null>();
+
+  const totalPrice = items.reduce(
+    (total, item) => total + item.price * item.qty,
+    0
+  );
 
   const uploadAndConfirm = () => {
     push("/order-status/123");
+  };
+
+  const handleConfirmPayment = () => {
+    if (!file) {
+      alert("Please upload your payment receipt");
+      return;
+    }
+
+    if (!customerInfo) {
+      alert("Customer information is missing, please return to checkout");
+      push("/checkout");
+      return;
+    }
+
+    try {
+      const formData = new FormData();
+      formData.append("customerName", customerInfo.customerName);
+      formData.append(
+        "customerContact",
+        customerInfo.customerContact!.toString()
+      );
+      formData.append("customerAddress", customerInfo.customerAddress);
+      formData.append("image", file);
+      formData.append(
+        "purchasedItems",
+        JSON.stringify(
+          items.map((item) => ({ productId: item._id, qty: item.qty }))
+        )
+      );
+      formData.append("totalPayment", totalPrice!.toString());
+    } catch (error) {
+      console.log(error);
+    }
+    uploadAndConfirm();
   };
 
   return (
@@ -33,13 +76,15 @@ const PaymentSteps = () => {
             transaction.
           </li>
         </ol>
-        <FileUpload />
+        <FileUpload onFileSelect={setFile} />
       </div>
 
       <div className="border-t border-gray-200 p-4">
         <div className="flex justify-between font-semibold">
           <div className="text-sm">Total</div>
-          <div className="text-primary text-xs">{priceFormatter(1035000)}</div>
+          <div className="text-primary text-xs">
+            {priceFormatter(totalPrice)}
+          </div>
         </div>
         <Button
           variant="dark"
--- app/(landing)/payment/page.tsx
+++ app/(landing)/payment/page.tsx
@@ -3,7 +3,7 @@ import PaymentSteps from "../components/payment/payment-steps";
 
 const Payment = () => {
   return (
-    <main className="bg-gray-100 min-h-[80vh]">
+    <main className="bg-gray-100 min-h-[80vh] pt-20">
       <div className="max-w-5xl mx-auto py-20">
         <h2 className="text-5xl font-bold text-center mb-11">Payment</h2>
         <div className="grid grid-cols-2 gap-14">

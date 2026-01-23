c6cff8887077151acfaf9b73543bd250b0582d58
frsyi <fahmarosyi@gmail.com>
2026-01-23 20:52:04 +0700

    feat: add product management page

--- app/(dashboard)/admin/products/page.tsx
+++ app/(dashboard)/admin/products/page.tsx
@@ -1,7 +1,21 @@
+import Button from "@/app/(landing)/components/ui/button";
+import { FiPlus } from "react-icons/fi";
+import ProductTable from "../../components/products/product-table";
+
 const ProductManagement = () => {
   return (
     <div>
-      <h1>Product Manager</h1>
+      <div className="flex justify-between items-center mb-10">
+        <div>
+          <h1 className="font-bold text-2xl">Product Management</h1>
+          <p className="opacity-50">Manage your inventory, prices and stock.</p>
+        </div>
+        <Button className="rounded-lg">
+          <FiPlus size={24} />
+          Add Product
+        </Button>
+      </div>
+      <ProductTable />
     </div>
   );
 };
